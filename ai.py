1df8ee1f86ef85eeee46fae1f870bf2abf5c2d18
Nicolas <nicolas@debian.home>
2022-03-02 12:29:11 +0100

    <tweak>: Change function repartition to have it more suited to our needs.

--- ai.py
+++ ai.py
@@ -1,1 +1,31 @@
-#!/usr/bin/env python3
+from game import *
+
+mat_points = [[500,-150,30,10,10,30,-150,500],[-150,-250,0,0,0,0,-250,-150],[30,0,1,2,2,1,0,30],[10,0,2,16,16,2,0,0],[10,0,2,16,16,2,0,0],[30,0,1,2,2,1,0,30],[-150,-250,0,0,0,0,-250,-150],[500,-150,30,10,10,30,-150,500]]
+
+# Retourne la valeur maximale jouable
+def max_points(currentPlayer, plateau):
+    L_cases = coups_possibles(currentPlayer, plateau)
+    n = len(L_cases)
+    L_cases_max = []
+    if n != 0:
+        max = mat_points[L_cases[0][0]][L_cases[0][1]]
+        for k in range(1, n):
+            if mat_points[L_cases[k]] > max:
+                max = mat_points[L_cases[k][0]][L_cases[k][1]]
+    return(max)
+
+# Retourne le poids d'un coup, à savoir la différence de points entre les deux coups des joueurs
+# Ici, le plateau sera en fait le plateau simulé après que l'IA a joué la case "case_jouée"
+def poids_coup(case_jouée, currentPlayer, plateau):
+    point_adverse = max_points(currentPlayer, plateau)
+    point_case_jouée = mat_points[case_jouée[0]][case_jouée[1]]
+    poids = point_adverse - point_case_jouée
+    return(poids)
+
+def liste_coups(currentPlayer, plateau):
+    cases_possibles_ia = coups_possibles(currentPlayer, plateau)
+    n = len(cases_possibles_ia)
+    if n != 0:
+        mat_coup_apres = plateau.copy()
+        jouer_coup(currentPlayer, matrixToString(cases_possibles_ia[0]), mat_coup_apres)
+        #A FINIR
---
+++ game.py
@@ -0,0 +1,159 @@
+nbLigne = 8
+nbColonne = 8
+
+# Initialise le plateau
+def init():
+    global nbLigne
+    global nbColonne
+    plateau = [[None for x in range(nbColonne)] for y in range(nbLigne)]
+    plateau[3][3] = 0
+    plateau[4][4] = 0
+    plateau[4][3] = 1
+    plateau[3][4] = 1
+
+    return plateau
+
+def debug():
+    plateau = [[0,0,0,0,0,1,1,1],[0,0,0,0,1,0,1,1],[0,0,0,0,0,1,1,0],[0,0,0,0,1,1,0,0],[0,0,0,0,1,0,0,0],[1,0,0,0,1,0,0,0],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,None]]
+    return plateau
+
+# Indique si la partie est fini
+def isGameOver(currentPlayer,plateau):
+    if len(coups_possibles(currentPlayer,plateau)) == 0 and len(coups_possibles((currentPlayer + 1)%2,plateau)) == 0:
+        return True
+    return False
+
+# Retourne le score de chaque joueur
+def playerScore(plateau):
+    score_white = 0
+    score_black = 0
+    for i in range(nbLigne):
+        for j in range(nbColonne):
+            if plateau[i][j] == 0:
+                score_white += 1
+            else:
+                if plateau[i][j] == 1:
+                    score_black += 1
+    return [score_white,score_black]
+
+# Affiche le plateau dans la console
+def affichage(joueur,plateau):
+    numLigne = 1
+    print("    a b c d e f g h")
+    for ligne in range(nbLigne):
+        print(str(numLigne) + " |", end=" ")
+        for colonne in range(nbColonne):
+            if plateau[ligne][colonne] != None:
+                if plateau[ligne][colonne] == 0:
+                    print("B", end=" ")
+                else:
+                    print("N", end=" ")
+            else:
+                if coup_valide(joueur,[ligne,colonne],plateau):
+                    print("*", end=" ")
+                else:
+                    print(".", end=" ")
+        if ligne == 0:
+            score = playerScore(plateau)
+            print("|\tScore: (Blancs) " + str(score[0]) + " vs " + str(score[1]) + " (Noirs)")
+        else:
+            if ligne == 2:
+                if joueur == 0:
+                    print("|\tTrait aux blancs")
+                else:
+                    print("|\tTrait aux noirs")
+            else:
+                print("|")
+        numLigne = numLigne + 1
+    print("")
+    return
+
+# Conversion d'un coup de forme string (ex: "a1") sous format matrice (ex: [0,0])
+def stringToMatrix(coupString):
+    return [int(coupString[1]) - 1, ord(coupString.upper()[0]) - ord('A')]
+
+# Conversion d'un coup de forme matrice (ex: [0,0]) sous format string (ex: "a1")
+def matrixToString(coupMatrix):
+    return chr(coupMatrix[1]+ ord('A')) + str(coupMatrix[0] + 1)
+
+# Indique si le coup est valide
+def coup_valide(joueur, coupMatrix, plateau):
+    global nbLigne
+    global nbColonne
+
+    if coupMatrix[0] < 0 or coupMatrix[0] >= 8 or coupMatrix[1] < 0 or coupMatrix[1] >= 8 or plateau[coupMatrix[0]][coupMatrix[1]] != None:
+        return False
+
+    voisins = []
+    for i in range(-1,2):
+        if coupMatrix[0] + i >= 0 and coupMatrix[0] + i < 8:
+            for j in range(-1,2):
+                if coupMatrix[1] + j >= 0 and coupMatrix[1] + j < 8:
+                    if not (i ==  0 and j == 0):
+                        voisins.append([coupMatrix[0] + i,coupMatrix[1] + j])
+
+    for voisin in voisins:
+        ligne = voisin[0] - coupMatrix[0]
+        colonne = voisin[1] - coupMatrix[1]
+
+        current_pawn_i = coupMatrix[0] + ligne
+        current_pawn_j = coupMatrix[1] + colonne
+        nb_pion = 0
+        while current_pawn_i >= 0 and current_pawn_i < 8 and current_pawn_j >= 0 and current_pawn_j < 8 and plateau[current_pawn_i][current_pawn_j] != joueur and plateau[current_pawn_i][current_pawn_j] != None:
+            current_pawn_i = current_pawn_i + ligne
+            current_pawn_j = current_pawn_j + colonne
+            nb_pion += 1
+            #print(current_pawn_i,current_pawn_j)
+            if current_pawn_i >= 0 and current_pawn_i < 8 and current_pawn_j >= 0 and current_pawn_j < 8 and plateau[current_pawn_i][current_pawn_j] == joueur:
+                return True
+    return False
+
+# Retourne tous les coups possibles
+def coups_possibles(currentPlayer,plateau):
+    coups = []
+    for i in range(nbLigne):
+        for j in range(nbColonne):
+            if coup_valide(currentPlayer,[i,j],plateau):
+                coups.append([i,j])
+    return coups
+
+# Retourne les pions récemment capturés
+def update_board(coupMatrix,plateau):
+    voisins = []
+    for i in range(-1,2):
+        if coupMatrix[0] + i >= 0 and coupMatrix[0] + i < 8:
+            for j in range(-1,2):
+                if coupMatrix[1] + j >= 0 and coupMatrix[1] + j < 8:
+                    if not (i ==  0 and j == 0) and plateau[coupMatrix[0] + i][coupMatrix[1] + j] != None:
+                        voisins.append([coupMatrix[0] + i,coupMatrix[1] + j])
+    
+    to_flip = []
+    temp = []
+    joueur = plateau[coupMatrix[0]][coupMatrix[1]]
+    for voisin in voisins:
+        ligne = voisin[0] - coupMatrix[0]
+        colonne = voisin[1] - coupMatrix[1]
+
+        current_pawn_i = coupMatrix[0] + ligne
+        current_pawn_j = coupMatrix[1] + colonne
+        nb_pion = 0
+        temp = [[current_pawn_i,current_pawn_j]]
+        while current_pawn_i >= 0 and current_pawn_i < 8 and current_pawn_j >= 0 and current_pawn_j < 8 and plateau[current_pawn_i][current_pawn_j] != joueur and plateau[current_pawn_i][current_pawn_j] != None:
+            current_pawn_i = current_pawn_i + ligne
+            current_pawn_j = current_pawn_j + colonne
+            nb_pion += 1
+            if current_pawn_i >= 0 and current_pawn_i < 8 and current_pawn_j >= 0 and current_pawn_j < 8 and plateau[current_pawn_i][current_pawn_j] == joueur:
+                for element in temp:
+                    to_flip.append(element)
+            temp.append([current_pawn_i,current_pawn_j])
+    for coup in to_flip:
+        plateau[coup[0]][coup[1]] = joueur
+        
+    #print([matrixToString(element) for element in to_flip])
+    return
+
+# Permet au joueur actuel de jouer un coup valide
+def jouer_coup(joueur, coupString, plateau):
+    coupMatrix = stringToMatrix(coupString)
+    plateau[coupMatrix[0]][coupMatrix[1]] = joueur
+    update_board(coupMatrix,plateau)
