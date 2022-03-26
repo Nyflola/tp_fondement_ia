38b5fc28ac9407bc43edd351d74d14b96c789623
achille magnin <achille.magnin@orange.fr>
2022-03-26 18:48:52 +0100

    fonction min max

--- graph.py
+++ graph.py
@@ -8,7 +8,7 @@ class Graphe: # L'objet graphe
 
     def __init__(self, EI, E, Adj, plateau): 
         self.EI = EI # EI = [etat initial 1]
-        self.E = E # E = [[indice etat, le plateau, le poids], ...]
+        self.E = E # E = [[indice etat, le plateau, le poids, le joueur qui joue], ...]
         self.Adj = Adj # Adj = [[successeurs de 1], [successeurs de 2], ...]
         self.g = [[], [], []]
         self.plateau = plateau
@@ -19,8 +19,8 @@ class Graphe: # L'objet graphe
     #self.EI = self.EI + [element]
     #self.g[0] = self.EI
 
-    def ajoutEtat(self, element, plateau, poids):
-        self.E = self.E + [[element, plateau, poids]]
+    def ajoutEtat(self, element, plateau, poids, joueur):
+        self.E = self.E + [[element, plateau, poids, joueur]]
         self.g[1] = self.E 
 
     def ajoutPredecesseur(self, element, pred): # element = element à  ajouter et pred = son predecesseur (indice)
@@ -61,6 +61,51 @@ class Graphe: # L'objet graphe
         if ia == "absolue" or (ia == "mixte" and ncoup > 43):
             return Graphe.evaluation_absolue(idsommet, coupMatrix, currentPlayer)
 
+    #renvoie la liste [[sommets de hauteur 1], [sommets de hauteur 2], ..., [sommets de hauteur n]]
+    def palier(self):
+        L_paliers = [[1]]
+        L_adj = self.g[2]
+        n = len(self.g[1]) #le  nombre de sommets du graphe
+        k = 1
+        while k < n:
+            L_paliers = L_paliers + [[]]
+            for i in range(len(L_paliers[-2])):
+                L_paliers[-1] = L_paliers[-1] + L_adj[L_paliers[-2][i]-1]
+                k += len(L_adj[L_paliers[-2][i]-1])
+        return L_paliers
+
+    def poids_liste(self, L):
+        pds = []
+        for k in range(len(L)):
+            pds = pds + [self.g[1][L[k]-1][2]]
+        return pds
+
+    #def predecesseur(self, sommet, graphe):
+    #    for i in range(graphe[2]):
+    #        for k in range(graphe[2][i]):
+    #            if graphe[2][i][k] == sommet:
+    #                return graphe[2][i][k]
+
+    def min_max(self, ia, joueurTour):
+        paliers = Graphe.palier()
+        tmax = False
+        n = len(paliers)
+        if ia == joueurTour:
+            tmax = True
+        if len(paliers) == 1:
+            return self.g[1][0][2]
+        else:
+            for k in range(len(paliers)-1):
+                for i in range(len(paliers[-(k+2)])):
+                    poids = Graphe.poids_liste(self.g[2][paliers[-(k+2)][i]-1])
+                    if len(poids>0):
+                        if ((n-k)/2 - (n-k)//2 == 0 and tmax == False) or (((n-k)/2 - (n-k)//2 != 0 and tmax)):
+                            self.g[1][paliers[-(k+2)][i]-1][2] = max(poids)
+                        else:
+                            self.g[1][paliers[-(k + 2)][i] - 1][2] = min(poids)
+
+
+
 
 
 
@@ -69,17 +114,17 @@ class Graphe: # L'objet graphe
 
     
 #print(P)
-EI = [1]
-E = [[1, [], None], [2, [], None], [3, [], None]] #le plateau en 2ème position
-Adj = [[2, 3], [], []]
+#EI = [1]
+#E = [[1, [], None], [2, [], None], [3, [], None]] #le plateau en 2ème position
+#Adj = [[2, 3], [], []]
 
-Graphe_othello = Graphe(EI, E, Adj)
-Graphe_othello.creation()
-graphe = Graphe_othello.g
+#Graphe_othello = Graphe(EI, E, Adj)
+#Graphe_othello.creation()
+#graphe = Graphe_othello.g
 
-print(graphe)
+#print(graphe)
 
-Graphe_othello.ajout(4, [], 1, None, False)
+#Graphe_othello.ajout(4, [], 1, None, False)
 
 #Graphe_othello.ajoutEtat(4, None)
 #Graphe_othello.ajoutPredecesseur(4, 1)
@@ -87,10 +132,10 @@ Graphe_othello.ajout(4, [], 1, None, False)
 #graphe = Graphe_othello.ajoutEtatInitial(4)
 #graphe = Graphe_othello.ajoutSuccesseur([1, 2, 3])
 
-graphe = Graphe_othello.g
-print(graphe)
+#graphe = Graphe_othello.g
+#print(graphe)
     
-Graphe_othello.modifierPoids([2, 4], [8, 9]) 
-graphe = Graphe_othello.g         
-print(graphe)
+#Graphe_othello.modifierPoids([2, 4], [8, 9])
+#graphe = Graphe_othello.g
+#print(graphe)
 
